62418f37146dd4f0fc064bb05f3d86560b49d0e9
三三 <oott123@users.noreply.github.com>
2021-09-09 13:14:56 +0800

    Update cloudinit-userdata.sh

--- cloudinit-userdata.sh
+++ cloudinit-userdata.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+apt-get update -y
+apt-get install -y wget
 wget -O /root/seinit.sh https://raw.githubusercontent.com/oott123/seinit/master/seinit.sh
 chmod +x /root/seinit.sh
 SEI_BACKUP=yes SEI_FUCK_SSH=yes /root/seinit.sh
